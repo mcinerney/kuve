6d59d9da6344056a9de7d1237751e78d63785091
Jay Gabriels <jgabriels30@gmail.com>
2018-01-05 17:26:15 -0800

    add help menu

--- kuve.cr
+++ kuve.cr
@@ -13,6 +13,10 @@ if ARGV[0] == "nodes"
 elsif ARGV[0] == "restarts"
   a = Restarts.new
   a.show_all_restarts
+elsif ARGV[0] == "-h"
+  puts "kuve <namespace> - shows all pods in a namespace for each project"
+  puts "kuve restarts - shows node and app restarts per project"
+  puts "kuve nodes - shows all warning and error messages for all nodes in a project"
 else
   na = Namespace.new
   na.get_all_pods_all_namespaces_all_envs
