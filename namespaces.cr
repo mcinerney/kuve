b2bd334c481b03c5cf9984cf235dd6be16a73556
Paul Chandler <pchandler@onemarketnetwork.com>
2018-01-08 15:44:59 -0800

    Fix bug when not deployed

--- namespaces.cr
+++ namespaces.cr
@@ -21,12 +21,19 @@ class Namespace
   def get_deployed_image(context)
     deployment_json = `kubectl get deployment -o=json #{namespace} --context=#{context}`
     deployment = JSON.parse(deployment_json)
-    deployment["items"].first["spec"]["template"]["spec"]["containers"].first["image"]
+
+    begin
+      deployment["items"].first["spec"]["template"]["spec"]["containers"].first["image"]
+    rescue
+      ""
+    end
   end
 
   def get_events(context)
     deployment_json = `kubectl get events -o=json #{namespace} --context=#{context}`
+
     deployment = JSON.parse(deployment_json)
+
     str = String.build do |str|
       deployment["items"].each do |item|
         if item["type"] == "Warning" || item["type"] == "Error"
@@ -49,24 +56,27 @@ class Namespace
       spawn do
         str = String.build do |str|
           image_info = (get_deployed_image ctx).to_s
-          sha = image_info.to_s.split(":").last
-          @data << sha
 
-          str << ("%*s%*s\n\n" % [60 + image_info.size / 2, image_info, 60 - image_info.size / 2, ""]).gsub(" ", "-")
+          if !image_info.empty?
+            sha = image_info.to_s.split(":").last
+            @data << sha
 
-          output = IO::Memory.new
-          Process.run "kubectl",
-            ["get", "pods", "-o", "wide", "#{namespace}", "--context", ctx], output: output
-          output.close
-          str << output.to_s
+            str << ("%*s%*s\n\n" % [60 + image_info.size / 2, image_info, 60 - image_info.size / 2, ""]).gsub(" ", "-")
 
-          event_info = get_events ctx
-          if !event_info.empty?
+            output = IO::Memory.new
+            Process.run "kubectl",
+              ["get", "pods", "-o", "wide", "#{namespace}", "--context", ctx], output: output
+            output.close
+            str << output.to_s
+
+            event_info = get_events ctx
+            if !event_info.empty?
+              str << "\n\n"
+              str << event_info
+            end
+
             str << "\n\n"
-            str << event_info
           end
-
-          str << "\n\n"
         end
 
         # send the results async to the receiver below
@@ -83,7 +93,7 @@ class Namespace
 
     # output the results by context, in order
     contexts.each do |ctx|
-      puts results[ctx]
+      printf results[ctx]
     end
 
     puts @data
