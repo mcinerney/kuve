13bd7015560e9c2b05c38394c004f57c6fceb544
Jay Gabriels <jgabriels30@gmail.com>
2018-01-20 11:18:24 -0800

    add db-con

--- README.md
+++ README.md
@@ -18,10 +18,19 @@ Easily view important debugging information over multiple environments.
 - $ brew update
 - $ brew install crystal-lang
 - $ cd kuve
+- $ cp kuve_conf.sample kuve_conf.json
+- $ Update your kuve_conf.json (see Steps to get db-con working)
 - $ make update
 
 ### List available commands
 - $ kuve -h
+
+## Steps to get db-con working
+- You will need to have all your apps in one directory
+- You will need to add a directory with all psql-service-accounts.json - this is needed to connect to the proxy
+- Update the kuve_conf.json to include these two paths
+- If the db-con fails to run correctly, check that you have the repo cloned to the apps directory and that you have the service account for the project in the right directory
+
 
 ### Todo
 - Use multiple contexts
--- kuve.cr
+++ kuve.cr
@@ -1,6 +1,7 @@
 require "./namespaces.cr"
 require "./restarts.cr"
 require "./nodes.cr"
+require "./db-con.cr"
 require "colorize"
 require "json"
 
@@ -26,16 +27,24 @@ if ARGV.size == 0 || ARGV[0] == "-h" || ARGV[0] == "h" || ARGV[0] == "--help"
   logo.stop
 
   puts ""
-  puts "$ kuve -h              shows this message"
-  puts "$ kuve <namespace>     shows all pods in a namespace for each project"
-  puts "$ kuve restarts        shows top six pod restarts in namespace and node"
-  puts "$ kuve restarts -a     shows all pod restarts in namespace and node"
-  puts "$ kuve nodes           shows all warning and error messages for all nodes in a project"
+  puts "$ kuve -h                           shows this message"
+  puts "$ kuve <namespace>                  shows all pods in a namespace for each project"
+  puts "$ kuve restarts                     shows top six pod restarts in namespace and node"
+  puts "$ kuve restarts -a                  shows all pod restarts in namespace and node"
+  puts "$ kuve nodes                        shows all warning and error messages for all nodes in a project"
+  puts "$ kuve db-con <project> <namespace> connects you to the cloud-sql-proxy for that namespace's db"
   puts ""
   puts ""
 elsif ARGV[0] == "nodes"
   no = Nodes.new
   no.get_all_nodes
+elsif ARGV[0] == "db-con"
+  if ARGV[1] && ARGV[2]
+    d = DBCon.new(ARGV[1], ARGV[2])
+    d.proxy
+  else
+    puts "Needs to be like $ kuve db-con <project> <namespace>"
+  end
 elsif ARGV[0] == "restarts"
   a = Restarts.new
   if (ARGV.size > 1) && (ARGV[1] == "-a" || ARGV[1] == "a")
